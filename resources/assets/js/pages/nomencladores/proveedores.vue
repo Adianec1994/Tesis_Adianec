9a1de650d463ec2c7b0410e72ab2f24e86b96101
Unknown <juliocesar.pg92@gmail.com>
2019-05-23 20:14:41 -0400

    generic crud code done

--- resources/assets/js/pages/nomencladores/proveedores.vue
+++ resources/assets/js/pages/nomencladores/proveedores.vue
@@ -1,127 +1,22 @@
 <template>
-  <div>
-    <v-toolbar
-      flat
-      color="white"
-    >
-      <v-toolbar-title>Proveedores</v-toolbar-title>
-      <v-divider
-        class="mx-2"
-        inset
-        vertical
-      ></v-divider>
-      <v-spacer></v-spacer>
-      <v-dialog
-        v-model="dialog"
-        max-width="500px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            color="primary"
-            dark
-            class="mb-2"
-            v-on="on"
-            v-on:click="isNewModel=true"
-          >Nuevo</v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
-
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <vue-form-generator
-                  :schema="schema"
-                  :model="editedItem"
-                  :options="options"
-                  :isNewModel="isNewModel"
-                  @model-updated="updateItem"
-                  @validated="formIsValid"
-                >
-                </vue-form-generator>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="primary"
-              dark
-              @click="saveItem"
-            >Guardar</v-btn>
-            <v-btn
-              color="blue darken-1"
-              flat
-              @click="close"
-            >Cancelar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
-    <v-data-table
-      :headers="headers"
-      :items="proveedores"
-      class="elevation-1"
-    >
-      <template v-slot:items="props">
-        <td>{{ props.item.marca }}</td>
-        <td class="text-xs-center">{{ props.item.serie }}</td>
-        <td class="text-xs-center">{{ props.item.pais }}</td>
-        <td class="text-xs-center justify-center">
-          <v-icon
-            small
-            class="mr-2"
-            @click="editItem(props.item)"
-          >
-            edit
-          </v-icon>
-          <v-icon
-            small
-            @click="$set(deleteDialog,props.item.id,true)"
-          >
-            delete
-          </v-icon>
-        </td>
-        <v-dialog
-          v-model="deleteDialog[props.item.id]"
-          :key="props.item.id"
-          persistent
-          max-width="290"
-        >
-          <v-card>
-            <v-card-title><b>Eliminar</b></v-card-title>
-            <v-card-text>{{`¿Seguro que desea eliminar el proveedor ${props.item.marca}`}}</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                dark
-                @click.stop="deleteItem(props.item)"
-              >Eliminar</v-btn>
-              <v-btn
-                color="blue darken-1"
-                flat
-                @click.stop="$set(deleteDialog,props.item.id,false)"
-              >Cancelar</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </template>
-      <template v-slot:no-data>
-        <h4>Sin datos para mostrar</h4>
-      </template>
-    </v-data-table>
-  </div>
+  <crudForm
+    :items="proveedores"
+    :headers="headers"
+    :schema="schema"
+    :options="options"
+    moduleName="Proveedores"
+  >
+  </crudForm>
 </template>
 
 <script>
-import Form from '../../components/form/Form'
+import crudForm from '../../components/crud'
 import { mapGetters } from 'vuex'
 
 export default {
+  components: {
+    crudForm
+  },
   data: () => ({
     dialog: false,
     headers: [
@@ -130,11 +25,7 @@ export default {
       { text: 'Pais', value: 'pais', align: 'center' },
       { text: 'Acciones', value: 'marca', sortable: false, align: 'center' }
     ],
-    deleteDialog: {},
     proveedores: [],
-    editedItem: {},
-    editedIndex: -1,
-    isNewModel: false,
     options: {
       validateAfterChanged: true,
       validateAfterLoad: true
@@ -168,24 +59,9 @@ export default {
     }
   }),
 
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'Nuevo' : 'Editar'
-    },
-    ...mapGetters({
-      proveedores: "get('proveedores')"
-    })
-  },
-
-  watch: {
-    dialog (val) {
-      val || this.close()
-    }
-  },
-
   mounted () {
     if (this.$store.getters.get('proveedores').length === 0) {
-      this.$store.dispatch('GET').then((result) => {
+      this.$store.dispatch('GET', 'proveedores').then((result) => {
         this.proveedores = this.$store.getters.get('proveedores')
         this.$store.dispatch('responseMessage', {
           type: result.success ? 'success' : 'error',
@@ -200,67 +76,6 @@ export default {
     } else {
       this.proveedores = this.$store.getters.get('proveedores')
     }
-  },
-
-  methods: {
-    editItem (item) {
-      this.editedIndex = this.proveedores.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    updateItem (newVal, property) {
-      // this.formIsValid()
-      this.editedItem[property] = newVal
-    },
-
-    saveItem () {
-      let response
-      if (this.editedIndex === -1) {
-        response = this.$store.dispatch('SAVE', this.editedItem)
-      } else {
-        response = this.$store.dispatch('EDIT', this.editedItem)
-      }
-
-      response.then(result => {
-        this.$store.dispatch('responseMessage', {
-          type: result.success ? 'success' : 'error',
-          text: result.message
-        })
-        this.close()
-      })
-    },
-
-    formIsValid (isValid, errors) {
-      // return isValid
-      // console.log('Results ', isValid, ', Errors ', errors)
-    },
-
-    deleteItem (item) {
-      this.$store.dispatch('DESTROY', item).then((result) => {
-        this.$store.dispatch('responseMessage', {
-          type: result.success ? 'success' : 'error',
-          text: result.message
-        })
-      }).catch((err) => {
-        this.$store.dispatch('responseMessage', {
-          type: 'error',
-          text: err
-        })
-      })
-    },
-
-    close () {
-      this.dialog = false
-      this.isNewModel = false
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, {})
-        this.editedIndex = -1
-      }, 300)
-    }
-  },
-  components: {
-    Form
   }
 }
 </script>
--- resources/assets/js/pages/nomencladores/provincias.vue
+++ resources/assets/js/pages/nomencladores/provincias.vue
@@ -1,136 +1,29 @@
 <template>
-  <div>
-    <v-toolbar
-      flat
-      color="white"
-    >
-      <v-toolbar-title>Provincias</v-toolbar-title>
-      <v-divider
-        class="mx-2"
-        inset
-        vertical
-      ></v-divider>
-      <v-spacer></v-spacer>
-      <v-dialog
-        v-model="dialog"
-        max-width="500px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            color="primary"
-            dark
-            class="mb-2"
-            v-on="on"
-            v-on:click="isNewModel=true"
-          >Nuevo</v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
-
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <vue-form-generator
-                  :schema="schema"
-                  :model="editedItem"
-                  :options="options"
-                  :isNewModel="isNewModel"
-                  @model-updated="updateItem"
-                  @validated="formIsValid"
-                >
-                </vue-form-generator>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="primary"
-              dark
-              @click="saveItem"
-            >Guardar</v-btn>
-            <v-btn
-              color="blue darken-1"
-              flat
-              @click="close"
-            >Cancelar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
-    <v-data-table
-      :headers="headers"
-      :items="provincias"
-      class="elevation-1"
-    >
-      <template v-slot:items="props">
-        <td>{{ props.item.nombre }}</td>
-        <td class="text-xs-center justify-center">
-          <v-icon
-            small
-            class="mr-2"
-            @click="editItem(props.item)"
-          >
-            edit
-          </v-icon>
-          <v-icon
-            small
-            @click="$set(deleteDialog,props.item.id,true)"
-          >
-            delete
-          </v-icon>
-        </td>
-        <v-dialog
-          v-model="deleteDialog[props.item.id]"
-          :key="props.item.id"
-          persistent
-          max-width="290"
-        >
-          <v-card>
-            <v-card-title><b>Eliminar</b></v-card-title>
-            <v-card-text>{{`¿Seguro que desea eliminar la provincia ${props.item.nombre}`}}</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                dark
-                @click.stop="deleteItem(props.item)"
-              >Eliminar</v-btn>
-              <v-btn
-                color="blue darken-1"
-                flat
-                @click.stop="$set(deleteDialog,props.item.id,false)"
-              >Cancelar</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </template>
-      <template v-slot:no-data>
-        <h4>Sin datos para mostrar</h4>
-      </template>
-    </v-data-table>
-  </div>
+  <crudForm
+    :items="provincias"
+    :headers="headers"
+    :schema="schema"
+    :options="options"
+    moduleName="Provincias"
+  >
+  </crudForm>
 </template>
 
 <script>
-import Form from '../../components/form/Form'
+import crudForm from '../../components/crud'
 import { mapGetters } from 'vuex'
 
 export default {
+  components: {
+    crudForm
+  },
   data: () => ({
     dialog: false,
     headers: [
       { text: 'Nombre', value: 'nombre' },
       { text: 'Acciones', value: 'marca', sortable: false, align: 'center' }
     ],
-    deleteDialog: {},
     provincias: [],
-    editedItem: {},
-    editedIndex: -1,
-    isNewModel: false,
     options: {
       validateAfterChanged: true,
       validateAfterLoad: true
@@ -148,25 +41,15 @@ export default {
   }),
 
   computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'Nuevo' : 'Editar'
-    },
-    ...mapGetters({
-      provincias: `get('provincias')`
-    })
-  },
-
-  watch: {
-    dialog (val) {
-      val || this.close()
-    }
+    // ...mapGetters({
+    //   provincias: `get('provincias')`
+    // })
   },
 
   mounted () {
     if (this.$store.getters.get('provincias').length === 0) {
       this.$store.dispatch('GET', 'provincias').then((result) => {
         this.provincias = this.$store.getters.get('provincias')
-        console.log('provincias ' + this.provincias)
         this.$store.dispatch('responseMessage', {
           type: result.success ? 'success' : 'error',
           text: result.message
@@ -180,67 +63,6 @@ export default {
     } else {
       this.provincias = this.$store.getters.get('provincias')
     }
-  },
-
-  methods: {
-    editItem (item) {
-      this.editedIndex = this.provincias.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    updateItem (newVal, property) {
-      // this.formIsValid()
-      this.editedItem[property] = newVal
-    },
-
-    saveItem () {
-      let response
-      if (this.editedIndex === -1) {
-        response = this.$store.dispatch('SAVE', { payload: this.editedItem, moduleName: 'provincias' })
-      } else {
-        response = this.$store.dispatch('EDIT', { payload: this.editedItem, moduleName: 'provincias' })
-      }
-
-      response.then(result => {
-        this.$store.dispatch('responseMessage', {
-          type: result.success ? 'success' : 'error',
-          text: result.message
-        })
-        this.close()
-      })
-    },
-
-    formIsValid (isValid, errors) {
-      // return isValid
-      // console.log('Results ', isValid, ', Errors ', errors)
-    },
-
-    deleteItem (item) {
-      this.$store.dispatch('DESTROY', { payload: item, moduleName: 'provincias' }).then((result) => {
-        this.$store.dispatch('responseMessage', {
-          type: result.success ? 'success' : 'error',
-          text: result.message
-        })
-      }).catch((err) => {
-        this.$store.dispatch('responseMessage', {
-          type: 'error',
-          text: err
-        })
-      })
-    },
-
-    close () {
-      this.dialog = false
-      this.isNewModel = false
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, {})
-        this.editedIndex = -1
-      }, 300)
-    }
-  },
-  components: {
-    Form
   }
 }
 </script>
