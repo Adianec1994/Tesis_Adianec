b2797b807e88598d8d518be11b4ab581a0ad8041
Unknown <juliocesar.pg92@gmail.com>
2019-09-03 16:37:23 -0400

    arreglos en actualizar potencia instalada

--- database/migrations/2019_09_01_152116_create_triggers.php
+++ database/migrations/2019_09_01_152116_create_triggers.php
@@ -13,60 +13,64 @@ class CreateTriggers extends Migration
      */
     public function up()
     {
-        DB::unprepared('
-            create procedure update_potencia_instalada (IN id INTEGER, IN tbl text)
-                begin
-                    declare potencia double;
-                    if tbl = "grupos" then
-                    select sum(potInstalada) into potencia from grupos where baterias_id = id;
-                    update baterias set potInstalada = potencia where baterias.id = id;
 
-                    elseif tbl = "baterias" then
-                        select sum(potInstalada) into potencia from baterias where central_electricas_id = id;
-                        update central_electricas set potInstalada = potencia where central_electricas.id = id;
+        DB::connection()->getPdo()->exec("
+        create procedure update_potencia_instalada_baterias (IN id INTEGER)
+            begin
+                declare potencia double;
+                select sum(potInstalada) into potencia from grupos where baterias_id = id;
+                update baterias set potInstalada = potencia where baterias.id = id;
+            end;
 
-                    elseif tbl = "central_electricas" then
-                        select sum(potInstalada) into potencia from central_electricas where entidads_id = id;
-                        update entidads set potInstalada = potencia where entidads.id = id;
+        create procedure update_potencia_instalada_ce (IN id INTEGER)
+            begin
+                declare potencia double;
+                select sum(potInstalada) into potencia from baterias where central_electricas_id = id;
+                update central_electricas set potInstalada = potencia where central_electricas.id = id;
+            end;
 
-                    end if;
+        create procedure update_potencia_instalada_entidads (IN id INTEGER)
+            begin
+                declare potencia double;
+                select sum(potInstalada) into potencia from central_electricas where entidads_id = id;
+                update entidads set potInstalada = potencia where entidads.id = id;
+            end;
+        ");
 
-                end;
-        ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_baterias_after_insert
-            after insert on grupos for each row call update_potencia_instalada(NEW.baterias_id, "grupos");
+            after insert on grupos for each row call update_potencia_instalada_baterias(NEW.baterias_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_baterias_after_update
-            after update on grupos for each row call update_potencia_instalada(NEW.baterias_id, "grupos");
+            after update on grupos for each row call update_potencia_instalada_baterias(NEW.baterias_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_baterias_after_delete
-            after delete on grupos for each row call update_potencia_instalada(OLD.baterias_id, "grupos");
+            after delete on grupos for each row call update_potencia_instalada_baterias(OLD.baterias_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_ce_after_update
-            after update on baterias for each row call update_potencia_instalada(NEW.central_electricas_id, "baterias");
+            after update on baterias for each row call update_potencia_instalada_ce(NEW.central_electricas_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_ce_after_delete
-            after delete on baterias for each row call update_potencia_instalada(OLD.central_electricas_id, "baterias");
+            after delete on baterias for each row call update_potencia_instalada_ce(OLD.central_electricas_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_entidads_after_update
-            after update on central_electricas for each row call update_potencia_instalada(NEW.entidads_id, "central_electricas");
+            after update on central_electricas for each row call update_potencia_instalada_entidads(NEW.entidads_id);
         ');
 
         DB::unprepared('
             create trigger update_potencia_instalada_entidads_after_delete
-            after delete on central_electricas for each row call update_potencia_instalada(OLD.entidads_id, "central_electricas");
+            after delete on central_electricas for each row call update_potencia_instalada_entidads(OLD.entidads_id);
         ');
     }
 
@@ -78,7 +82,9 @@ class CreateTriggers extends Migration
     public function down()
     {
         DB::connection()->getPdo()->exec("
-        DROP procedure update_potencia_instalada;
+        DROP procedure update_potencia_instalada_baterias;
+        DROP procedure update_potencia_instalada_ce;
+        DROP procedure update_potencia_instalada_entidads;
         ");
     }
 }
