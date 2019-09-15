0bde7df6c97f5f37bb426cfc345573d1aaad3614
Unknown <unknown@example.com>
2019-09-15 14:30:44 -0400

    EntidadTableSeeder sin datos de potencia
    Se quito los datos de potencia del seeder pq son datos calculables

--- database/seeds/EntidadTableSeeder.php
+++ database/seeds/EntidadTableSeeder.php
@@ -12,10 +12,10 @@ class EntidadTableSeeder extends Seeder
     public function run()
     {
             DB::table('entidads')->insert(['nombre'=>'Geysel Habana',
-            'potInstalada'=>20,'ip'=>'10.0.0.1','provincias_id'=>1]);
+            'ip'=>'10.0.0.1','provincias_id'=>1]);
             DB::table('entidads')->insert(['nombre'=>'Geysel Matanzas',
-            'potInstalada'=>18,'ip'=>'10.0.0.2','provincias_id'=>2]);
+            'ip'=>'10.0.0.2','provincias_id'=>2]);
             DB::table('entidads')->insert(['nombre'=>'Geysel Villa Clara',
-            'potInstalada'=>15,'ip'=>'10.0.0.3','provincias_id'=>3]);
+            'ip'=>'10.0.0.3','provincias_id'=>3]);
     }
 }
