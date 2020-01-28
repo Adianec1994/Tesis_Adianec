09ef1a65c2f9092a11b801706481673fc4d7a30a
Adianec1994 <adianec94@gmail.com>
2020-01-28 16:25:09 -0600

    nueva vista de inicio y color del menu

--- resources/assets/js/pages/home.vue
+++ resources/assets/js/pages/home.vue
@@ -1,11 +1,17 @@
 <template>
   <v-card>
-    <v-card-title class="grey lighten-4">
-      <h3 class="headline mb-0">{{ $t('home') }}</h3>
+    <v-card-title class="grey lighten-1">
+      <h3 class="display-1 mb-0">{{ $t('Bienvenido') }}</h3>
     </v-card-title>
     <v-divider></v-divider>
     <v-card-text>
-      {{ $t('you_are_logged_in') }}
+      <template>
+        <v-carousel cycle height="390" hide-delimiter-background show-arrows-on-hover>
+          <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"
+              reverse-transition="fade-transition" transition="fade-transition">
+          </v-carousel-item>
+        </v-carousel>
+      </template>
     </v-card-text>
   </v-card>
 </template>
@@ -15,6 +21,33 @@ export default {
   name: 'home-view',
   metaInfo () {
     return { title: this.$t('home') }
-  }
+  },
+  data (){
+    return{
+      items: [
+        {
+          src:'/img/geysel1.jpg',
+        },
+        {
+          src:'/img/geysel2.jpg',
+        },
+        {
+          src:'/img/geysel3.jpg',
+        },
+        {
+          src:'/img/geysel4.jpg',
+        },
+        {
+          src:'/img/geysel5.jpg',
+        },
+        {
+          src:'/img/geysel6.jpg',
+        },
+        {
+          src:'/img/geysel7.jpg',
+        },
+      ],
+    }
+  },
 }
 </script>
